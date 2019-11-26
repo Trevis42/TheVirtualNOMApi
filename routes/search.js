3e2a2587e054bbe8f8aafef7bf0e252ac9b9389c
TrevorTaylor-TP <ttaylor@talentpath.com>
2019-11-26 10:35:54 -0600

    added comments and cleared up some comments

--- routes/search.js
+++ routes/search.js
@@ -1,15 +1,15 @@
-'use strict';
- 
+"use strict";
+
 var fs = require("fs");
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-// import '../queryResults.json';
-var json = require('../queryResult.json');
+var json = require("../queryResult.json");
 
+const yelp = require("yelp-fusion");
+const client = yelp.client(
+  "UAoRvhpi0PKNClh1pDgb9OEKWeilv0DerV1lqHeaRWPjPXTkDQd3xfcg2gnOX2wAkpNL5yITDnlQzyrGc5CVTSrKo0U75xEkTg2O6Go4IXzzTMBkNuKVG1iiunDAXXYx "
+);
 
-const yelp = require('yelp-fusion');
-const client = yelp.client('UAoRvhpi0PKNClh1pDgb9OEKWeilv0DerV1lqHeaRWPjPXTkDQd3xfcg2gnOX2wAkpNL5yITDnlQzyrGc5CVTSrKo0U75xEkTg2O6Go4IXzzTMBkNuKVG1iiunDAXXYx ');
- 
 // client.search({
 // //   term: 'Four Barrel Coffee',
 // radius: 20000,
@@ -31,45 +31,48 @@ const client = yelp.client('UAoRvhpi0PKNClh1pDgb9OEKWeilv0DerV1lqHeaRWPjPXTkDQd3
 //   console.log(e);
 // });
 
-
-
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get("/", function(req, res, next) {
   let addy = req.query.location;
-  console.log('addy:', addy)
+  console.log("addy:", addy);
   // console.log("loc: ", address, "| radius:", rad)
-  console.log("loc: ", req.query.location, "| radius:", req.query.radius)
-  client.search({
-    //   term: 'Four Barrel Coffee',
-    // radius: 20000,
-    //   location: 'lat: 29.757241399999998, long:-95.46730649999999',
-    // latitude: 29.757241399999998,
-    // longitude: -95.46730649999999,
-    location : req.query.location,
-    radius : req.query.radius,
-    // console.log("loc: ", location, "| radius:", radius)
-    
-    
-    }).then(response => {
-        fs.writeFile("./queryResult.json", JSON.stringify(response.jsonBody.businesses), (err) => {
-            if (err) {
-                console.error(err);
-                return;
-            };
-            console.log("File has been created");
-        });
-        res.send(response.jsonBody.businesses);
+  console.log("loc: ", req.query.location, "| radius:", req.query.radius);
+  //* This is where we fetch the data...
+  //? Need to pass in the info from front end to get the right info
+  //? Need to figure out how to get lat/Long from zip
+  client
+    .search({
+      //   term: 'Four Barrel Coffee',
+      // radius: 20000,
+      //   location: 'lat: 29.757241399999998 long:-95.46730649999999',
+      // latitude: 29.757241399999998,
+      // longitude: -95.46730649999999,
+      location: req.query.location,
+      radius: req.query.radius,
+      // console.log("loc: ", location, "| radius:", radius)
+    })
+    .then(response => {
+      fs.writeFile(
+        "./queryResult.json",
+        JSON.stringify(response.jsonBody.businesses),
+        err => {
+          if (err) {
+            console.error(err);
+            return;
+          }
+          console.log("File has been created");
+        }
+      );
+      res.send(response.jsonBody.businesses);
       console.log(response.jsonBody.businesses[0].name);
-    }).catch(e => {
+    })
+    .catch(e => {
       console.log(e);
     });
   // res.send(json);
 });
 
 module.exports = router;
-
-
-
